11b1ede83835e3382653cfeb545eb51ad60f5d8b
DiepVanTy <tydv2409@gmail.com>
2023-09-26 20:57:57 +0700

    push to gitgub

--- app/src/main/java/com/man293/food_ordering_spoon/fragments/CartFragment.java
+++ app/src/main/java/com/man293/food_ordering_spoon/fragments/CartFragment.java
@@ -2,12 +2,17 @@ package com.man293.food_ordering_spoon.fragments;
 
 import android.os.Bundle;
 
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageButton;
 
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.man293.food_ordering_spoon.R;
 
 public class CartFragment extends Fragment {
@@ -17,6 +22,21 @@ public class CartFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_cart, container, false);
 
+        ImageButton deleteButton = view.findViewById(R.id.delete_button);
+        deleteButton.setOnClickListener(v -> {
+           showDialog();
+        });
+
         return view;
+    }
+
+    private void showDialog( ) {
+        BottomSheetDialog deleteDialog  = new BottomSheetDialog(getContext() , R.style.bottom_sheet_dialog_theme);
+        deleteDialog.setContentView(R.layout.dialog_confirm);
+        ImageButton closeButton = deleteDialog.findViewById(R.id.close_dialog_button);
+        closeButton.setOnClickListener(v -> {
+            deleteDialog.hide();
+        });
+        deleteDialog.show();
     }
 }
