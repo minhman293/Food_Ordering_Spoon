c5f892a686a53e96498dbbd960e7c02ab8188a4c
ngochao2503 <ngcha763@gmail.com>
2023-09-30 21:04:07 +0700

    up fragment product

---
+++ app/src/main/java/fragment/fragment_PRODUCT.java
@@ -0,0 +1,16 @@
+package fragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.man293.food_ordering_spoon.R;
+
+public class fragment_PRODUCT extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment_product);
+    }
+}
