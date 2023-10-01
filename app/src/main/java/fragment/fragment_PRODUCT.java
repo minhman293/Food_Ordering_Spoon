d17a39e141686c077c1564156c14132f849c355f
ngochao2503 <ngcha763@gmail.com>
2023-10-01 10:18:23 +0700

    finished fragment_Product

--- app/src/main/java/fragment/fragment_PRODUCT.java
+++ app/src/main/java/fragment/fragment_PRODUCT.java
@@ -3,14 +3,46 @@ package fragment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.man293.food_ordering_spoon.R;
 
 public class fragment_PRODUCT extends AppCompatActivity {
 
+    Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment_product);
+        button=(Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Creating the LayoutInflater instance
+                LayoutInflater li = getLayoutInflater();
+                //Getting the View object as defined in the customtoast.xml file
+                View layout = li.inflate(R.layout.custom_toast,(ViewGroup) findViewById(R.id.custom)); // id của LinearLayout trong custom_toast.xml
+
+                //set a textview at runtime to the custom toast textview
+                TextView text =(TextView)layout.findViewById(R.id.text);
+
+                //Creating the Toast object
+                Toast toast = new Toast(getApplicationContext());
+                toast.setDuration(Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP |Gravity.FILL_HORIZONTAL,0,20); // chỉnh
+                toast.setView(layout);
+                toast.show();
+            }
+        });
+
+
     }
 }
