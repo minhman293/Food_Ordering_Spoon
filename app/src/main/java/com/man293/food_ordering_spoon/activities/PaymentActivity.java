e47a809223e0fda1686805cde6e71cf133a16cc8
DiepVanTy <tydv2409@gmail.com>
2023-10-01 09:01:37 +0700

    completed thanks dialog

--- app/src/main/java/com/man293/food_ordering_spoon/activities/PaymentActivity.java
+++ app/src/main/java/com/man293/food_ordering_spoon/activities/PaymentActivity.java
@@ -1,11 +1,14 @@
 package com.man293.food_ordering_spoon.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.man293.food_ordering_spoon.R;
 
 public class PaymentActivity extends AppCompatActivity {
@@ -15,8 +18,28 @@ public class PaymentActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
 
+
+        /** GO BACK WHEN CLICK ARROW LEFT BUTTON */
+        ((ImageButton) findViewById(R.id.goBackButton)).setOnClickListener( v -> {
+            super.onBackPressed();
+            onBackPressed();
+        });
+
+        /** SELECT PAYMENT METHOD */
         handleSelectPaymentMethod();
         handleUnselectOtherPaymentMethod();
+
+        /** COMPLETE ORDER  */
+        AppCompatButton buyNowButton =  findViewById(R.id.buyNowButton);
+        buyNowButton.setOnClickListener( v -> {
+            BottomSheetDialog dialog = new BottomSheetDialog(PaymentActivity.this, R.style.bottom_sheet_dialog_theme);
+            dialog.setContentView(R.layout.dialog_thanks);
+            dialog.show();
+
+            ((ImageButton) dialog.findViewById(R.id.close_dialog_button)).setOnClickListener( view -> {
+                dialog.hide();
+            });
+        });
     }
 
     /**
