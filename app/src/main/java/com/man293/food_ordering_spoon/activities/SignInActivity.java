905cc53776b60be7c0f72f43650fffbe12994bc6
DiepVanTy <tydv2409@gmail.com>
2023-10-03 13:08:57 +0700

    refactor signin & login activity xml

--- app/src/main/java/com/man293/food_ordering_spoon/activities/SignInActivity.java
+++ app/src/main/java/com/man293/food_ordering_spoon/activities/SignInActivity.java
@@ -14,7 +14,7 @@ public class SignInActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.signin_activity);
+        setContentView(R.layout.activity_signin);
 
         configureBackButton();
     }
