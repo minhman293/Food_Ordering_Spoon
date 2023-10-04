fae84898ff736ad08319e06f5123b838dea424b6
DiepVanTy <tydv2409@gmail.com>
2023-10-04 09:28:19 +0700

    ViewFlipper issue has not been resolved

--- app/src/main/java/com/man293/food_ordering_spoon/fragments/HomeFragment.java
+++ app/src/main/java/com/man293/food_ordering_spoon/fragments/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.man293.food_ordering_spoon.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,8 +8,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import com.man293.food_ordering_spoon.R;
+import com.man293.food_ordering_spoon.activities.ProductActivity;
 
 /**TODO: TRUONG MINH MAN & LE NGOC HAO  */
 
@@ -19,6 +22,13 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view =  inflater.inflate(R.layout.fragment_home, container, false);
+
+        /** START PRODUCT ACTIVITY  */
+        RelativeLayout productCard1 = view.findViewById(R.id.product1);
+        productCard1.setOnClickListener(v -> {
+            startActivity(new Intent(getContext(), ProductActivity.class));
+        });
+
         return view;
     }
 }
