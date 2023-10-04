4137b6fd7b62f6b909b9193eea8c874866272082
KimNam2809 <lekimnam2809@gmail.com>
2023-10-04 20:19:23 +0700

    up them view history

---
+++ app/src/main/java/com/man293/food_ordering_spoon/activities/ViewHistory.java
@@ -0,0 +1,16 @@
+package com.man293.food_ordering_spoon.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.man293.food_ordering_spoon.R;
+
+public class ViewHistory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_history);
+    }
+}
