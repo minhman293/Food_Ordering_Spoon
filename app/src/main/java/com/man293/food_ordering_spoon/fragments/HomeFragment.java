a802e51d2878177bcd8ceefe9d4f605ef5abd053
MinhMan <mantruong293@gmail.com>
2023-10-12 17:07:47 +0700

    update minhman_UI Intro HomeFragment

--- app/src/main/java/com/man293/food_ordering_spoon/fragments/HomeFragment.java
+++ app/src/main/java/com/man293/food_ordering_spoon/fragments/HomeFragment.java
@@ -2,21 +2,100 @@ package com.man293.food_ordering_spoon.fragments;
 
 import android.os.Bundle;
 
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.man293.food_ordering_spoon.R;
 
 public class HomeFragment extends Fragment {
+
+    AppCompatButton btnCatAll, btnCatBeef, btnCatPizza, btnCatNoodle;
+    TextView txtDetail;
+
     public HomeFragment() { }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view =  inflater.inflate(R.layout.fragment_home, container, false);
+
+        btnCatAll = view.findViewById(R.id.btn_category_all);
+        btnCatBeef = view.findViewById(R.id.btn_category_beef);
+        btnCatNoodle = view.findViewById(R.id.btn_category_noodles);
+        btnCatPizza = view.findViewById(R.id.btn_category_pizza);
+
+        btnCatAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleClickCategory(v);
+
+                Unclick(btnCatBeef);
+                Unclick(btnCatNoodle);
+                Unclick(btnCatPizza);
+            }
+        });
+
+        btnCatPizza.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleClickCategory(v);
+                txtDetail = view.findViewById(R.id.txt_detail);
+                txtDetail.setText("Results for pizza");
+
+                Unclick(btnCatBeef);
+                Unclick(btnCatAll);
+                Unclick(btnCatNoodle);
+            }
+        });
+
+        btnCatBeef.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleClickCategory(v);
+                txtDetail = view.findViewById(R.id.txt_detail);
+                txtDetail.setText("Results for beef");
+
+                Unclick(btnCatPizza);
+                Unclick(btnCatAll);
+                Unclick(btnCatNoodle);
+            }
+        });
+
+        btnCatNoodle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleClickCategory(v);
+                txtDetail = view.findViewById(R.id.txt_detail);
+                txtDetail.setText("Results for noodle");
+
+                Unclick(btnCatBeef);
+                Unclick(btnCatAll);
+                Unclick(btnCatPizza);
+            }
+        });
+
         return view;
     }
+
+    public void Unclick(View v){
+        Button btn = (Button) v;
+        int textColor = ContextCompat.getColor(v.getContext(), R.color.green);
+        btn.setTextColor(textColor);
+        btn.setBackground(getResources().getDrawable(R.drawable.bounder_btn_green));
+    }
+
+    public void handleClickCategory(View v){
+        Button btn = (Button) v;
+        int textColor = ContextCompat.getColor(v.getContext(), R.color.white);
+        btn.setTextColor(textColor);
+        btn.setBackground(getResources().getDrawable(R.drawable.bounder_btn_category_hover));
+    };
 }
