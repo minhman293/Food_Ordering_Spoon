88137fa4fdda0b0536fb25b07e795390e5848b24
binh123440 <binhgolden123@gmail.com>
2023-10-24 21:46:33 +0700

    dieuchinhcochu

--- app/src/main/java/com/example/spoon_orderdetail_admin/order_detail_admin.java
+++ app/src/main/java/com/example/spoon_orderdetail_admin/order_detail_admin.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class order_detail_admin extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.order_detail_admin);
     }
 }
