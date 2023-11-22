acf43b8c5ea5813c7cc37d0cd126cdc686fac22b
MinhMan <mantruong293@gmail.com>
2023-11-22 15:14:58 +0700

    listview home product

--- app/src/main/java/com/man293/food_ordering_spoon/fragments/HomeFragment.java
+++ app/src/main/java/com/man293/food_ordering_spoon/fragments/HomeFragment.java
@@ -7,49 +7,82 @@ import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
 
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 import com.man293.food_ordering_spoon.R;
 import com.man293.food_ordering_spoon.activities.ProductActivity;
+import com.man293.food_ordering_spoon.adapters.HomeProductAdapter;
+import com.man293.food_ordering_spoon.components.ListViewComponent;
+import com.man293.food_ordering_spoon.models.HomeProduct;
 
-/**TODO: TRUONG MINH MAN & LE NGOC HAO  */
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**TODO: TRUONG MINH MAN */
 
 public class HomeFragment extends Fragment {
 
     AppCompatButton btnCatAll, btnCatBeef, btnCatPizza, btnCatNoodle;
     TextView txtDetail;
-    RelativeLayout rltLayProd1;
+    ListViewComponent lvHomeProduct;
+    ArrayList<HomeProduct> arrHomeProduct;
+    HomeProductAdapter homeProductAdapter;
+    View view;
     public HomeFragment() { }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view =  inflater.inflate(R.layout.fragment_home, container, false);
+        view =  inflater.inflate(R.layout.fragment_home, container, false);
 
-        rltLayProd1 = view.findViewById(R.id.product1);
-        rltLayProd1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getContext(), ProductActivity.class);
-                startActivity(intent);
-            }
-        });
+        // initialize
+        initialize();
 
+        // add data to array product
+        arrHomeProduct.add(new HomeProduct("Spaghetti", "It is a established fact a established that a..", 24, R.drawable.product_1));
+        arrHomeProduct.add(new HomeProduct("Spaghetti", "It is a established fact a established that a..", 24, R.drawable.product_2));
+        arrHomeProduct.add(new HomeProduct("Spaghetti", "It is a established fact a established that a..", 24, R.drawable.product_3));
+        arrHomeProduct.add(new HomeProduct("Spaghetti", "It is a established fact a established that a..", 24, R.drawable.product_4));
+
+        // set adapter for listview
+        lvHomeProduct.setAdapter(homeProductAdapter);
+        lvHomeProduct.setFullHeight();
+
+        // handle category button click
+        handleAllCategoryClick();
+
+        return view;
+    }
+
+    private void initialize() {
+        lvHomeProduct = (ListViewComponent) view.findViewById(R.id.lvHomeProduct);
 
         btnCatAll = view.findViewById(R.id.btn_category_all);
         btnCatBeef = view.findViewById(R.id.btn_category_beef);
         btnCatNoodle = view.findViewById(R.id.btn_category_noodles);
         btnCatPizza = view.findViewById(R.id.btn_category_pizza);
 
+        // array to contain list product
+        arrHomeProduct = new ArrayList<>();
+
+        // set adapter for list view product
+        homeProductAdapter = new HomeProductAdapter(getContext(), R.layout.single_home_product, arrHomeProduct);
+    }
+
+    private void handleAllCategoryClick() {
         btnCatAll.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -100,8 +133,6 @@ public class HomeFragment extends Fragment {
             }
 
         });
-
-        return view;
     }
 
     public void Unclick(View v){
